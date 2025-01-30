6c1486fcd033c1d239279f3044099a45ce42314f
Rafal Krupinski <oeklo.it@fastmail.com>
2025-01-30 15:56:26 +0100

    ✅ Add test file for merge event.

--- tests/test_parse.py
+++ tests/test_parse.py
@@ -9,7 +9,8 @@ def test_parse():
     for p in Path("tests").glob("*.json"):
         msgspec.json.decode(p.read_bytes(), type=Event_)
 
+
 def test_parse_all():
-    data = '['+','.join(p.read_text() for p in Path("tests").glob("*.json"))+']'
+    data = "[" + ",".join(p.read_text() for p in Path("tests").glob("*.json")) + "]"
     model = msgspec.json.decode(data, type=Message)
     assert model
